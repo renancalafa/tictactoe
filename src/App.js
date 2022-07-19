c6c629f316a9b30cb984b8d2069ec127ef5189a5
renancalafa <renancalafange@gmail.com>
2022-07-19 11:14:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let symbol = 0
+
+  const shoot = () => {
+    alert(symbol ? 'O' : 'X')
+    symbol = symbol ? 0 : 1
+    
+  }
+   
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="grid-container">
+          <div class="grid-item gi1" onClick={() => shoot()}></div>
+          <div class="grid-item gi2" onClick={() => shoot()}></div>
+          <div class="grid-item gi3" onClick={() => shoot()}></div>  
+          <div class="grid-item gi4" onClick={() => shoot()}></div>
+          <div class="grid-item gi5" onClick={() => shoot()}></div>
+          <div class="grid-item gi6" onClick={() => shoot()}></div>  
+          <div class="grid-item gi7" onClick={() => shoot()}></div>
+          <div class="grid-item gi8" onClick={() => shoot()}></div>
+          <div class="grid-item gi9" onClick={() => shoot()}></div>  
+        </div>
       </header>
     </div>
   );
